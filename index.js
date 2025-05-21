ef6c62d63eb2f5e7efa0dd532184ce15282f44b8
UmDangar <umesh.dangar@metizsoft.com>
2025-05-21 12:32:25 +0000

    added my new route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   return res.send("Hello World!");
 });
 
+app.get("/test", (req, res) => res.send("This is a test route for the app!"));
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
